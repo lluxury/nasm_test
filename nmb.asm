3c60ea8a89effa86e175e216961fa400e5a90074
yann <tmp4321@qq.com>
2019-05-22 11:04:08 +0800

    add reg
    Signed-off-by: yann <tmp4321@qq.com>

--- nmb.asm
+++ nmb.asm
@@ -2,10 +2,12 @@ global main
 
 main:
     mov eax, 1
-    add eax, 2
-    add eax, 3
-    add eax, 4
-    add eax, 5
+    mov ebx, 2
+    mov ecx, 3
+    mov edx, 4
+    add eax, ebx
+    add eax, ecx
+    add eax, edx
     ret
 
 
