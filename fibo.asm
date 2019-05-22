ce362937301cbc24f7f7eb26ecd8e6436a912536
yann <tmp4321@qq.com>
2019-05-22 18:43:55 +0800

    wfibo err
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ fibo.asm
@@ -0,0 +1,23 @@
+fibo:
+    cmp eax, 1
+    je _get_out
+    cmp eax, 2
+    je _get_out
+    
+    mov edx, eax
+    sub eax, 1
+    call fibo
+    mov ebx, eax
+    
+    mov eax, edx
+    sub eax, 2
+    call fibo
+    mov ecx, eax
+    
+    mov eax, ebx
+    add eax, ecx
+    ret
+    
+_get_out:
+    mov eax, 1
+    ret
